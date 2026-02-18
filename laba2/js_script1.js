bee862fff2bebcbe2c8e4e732ef2a62bc70c9dcc
VadymGG <burkut.vadym@krfk.kai.edu.ua>
2026-02-18 14:14:04 +0200

    laba2

---
+++ laba2/js_script1.js
@@ -0,0 +1,28 @@
+// Завдання 1
+var one = "adas";
+let two = 20;
+const there = 5;
+
+// Завдання 2
+console.log(typeof one)
+console.log(typeof two)
+console.log(typeof there)
+
+// Завдання 3
+let student = {
+    name: "vadym",
+    year: 18,
+    student: true
+}
+console.log(student)
+
+// Завдання 4
+student.university = "KRFK KAI"
+console.log(student)
+
+// Завдання 5
+two = "google";
+console.log(two)
+
+// Завдання 6
+there = "gg";
