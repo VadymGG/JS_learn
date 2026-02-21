de89b27ecd8cf4b891466abd8911f4c537329b3c
VadymGG <burkut.vadym@krfk.kai.edu.ua>
2026-02-21 22:49:52 +0200

    dop

---
+++ laba4/js_script1.js
@@ -0,0 +1,45 @@
+// Завдання 1
+let temperature = 10
+if (temperature >= 20 && temperature <= 25) {
+    console.log("Комфортна погода")
+} else if (temperature >= 26) {
+    console.log("Спекотна погода")
+} else {
+    console.log("Холодна погода")
+}
+
+// Завдання 2
+number = 3
+let N = (number % 2 == 0) ? "Парне" : "Не парне" 
+console.log(N)
+
+// Завдання 3
+let day = "Середа"
+switch (day) {
+    case "Понеділок" :
+        console.log("Перший день тижня")
+        break
+    case "Вівторок" :
+        console.log("Другий день тижня")
+        break
+    case "Середа" :
+        console.log("Третій день тижня")
+        break
+    case "Четвер" :
+        console.log("Четвертий день тижня")
+        break
+    case "П'ятниця" :
+        console.log("П'ятий день тижня")
+        break
+    case "Субота" :
+        console.log("Шостий день тижня")
+        break
+    case "Неділя" :
+        console.log("Сьомий день тижня")
+        break
+}
+
+// Завдання 4
+let password = "admin12tyjh3"
+let check = (password == "admin123") ? "Доступ дозволено" : "Доступ заборонено"
+console.log(check)
