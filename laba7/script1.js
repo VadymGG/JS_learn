e98d58f3d3ba3c18c10b4d1ed9504f515d44d974
VadymGG <burkut.vadym@krfk.kai.edu.ua>
2026-03-21 11:20:35 +0200

    lab7

---
+++ laba7/script1.js
@@ -0,0 +1,22 @@
+let messengers = ["Signal", "Instagram", "Telegram"];
+let numbers = [42, 15, 8, 99]; 
+
+// sort() та reverse()
+numbers.sort((a, b) => a - b);
+console.log("Відсортовані числа:", numbers);
+console.log("Месенджери задом наперед:", messengers.reverse());
+
+// concat() та slice()
+let all_Messengers = messengers.concat(["Viber", "WhatsApp"]);
+console.log("Об'єднаний масив:", all_Messengers);
+console.log("Частина масиву (slice):", all_Messengers.slice(0, 2));
+
+// splice()
+all_Messengers.splice(1, 1, "Discord"); 
+console.log(all_Messengers);
+
+// indexOf(), includes()
+console.log("Індекс WhatsApp:", all_Messengers.indexOf("WhatsApp")); 
+console.log("Чи є Skype?", all_Messengers.includes("Skype"));
+
+
