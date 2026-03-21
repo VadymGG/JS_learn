415d6c1f725c15662fc81b14f819803ef24e0e46
VadymGG <burkut.vadym@krfk.kai.edu.ua>
2026-03-21 11:43:23 +0200

    lab8

---
+++ laba8/script1.js
@@ -0,0 +1,20 @@
+function one() {
+    console.log("Функція one");
+}
+
+one();
+
+function number(apple) {
+    console.log("В мене " + apple + " яблук");
+}
+
+number(10);
+number(5);
+
+// Варіант №5
+function subtract(a, b) {
+    return a - b; 
+}
+
+let result = subtract(10, 4);
+console.log(result);
---
+++ laba8/task+.js
@@ -0,0 +1,4 @@
+function getFirstCharacter(str) {
+    return str[0]; 
+}
+console.log(getFirstCharacter("JS")); 
